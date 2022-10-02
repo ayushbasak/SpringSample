617206d52d935ebd40d34ea5bb72bda3c68fb8c7
Ayush Basak <ayushbasak0210@gmail.com>
2022-10-02 23:32:15 +0530

    Final Commit
    complete GET POST DELETE and PUT requests

--- src/main/java/com/basak/SpringSample/SpringSampleApplication.java
+++ src/main/java/com/basak/SpringSample/SpringSampleApplication.java
@@ -2,12 +2,14 @@ package com.basak.SpringSample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class SpringSampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSampleApplication.class, args);
 	}
-
 }
